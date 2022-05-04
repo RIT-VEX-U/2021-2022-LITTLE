dfbf30dc1885d910cf204c60e8536e6bbb5f1f7d
Ryan McGee <ramtech51@comcast.net>
2022-05-03 19:59:58 -0500

    Competition vision stuffs

--- include/vision_config.h
+++ include/vision_config.h
@@ -2,10 +2,10 @@
 #include "vex.h"
 vex::vision::signature RED_GOAL = vex::vision::signature (1, 8757, 9633, 9196, -985, -707, -846, 7.9, 0);
 vex::vision::signature BLUE_GOAL = vex::vision::signature (2, -2937, -2653, -2796, 10741, 11565, 11154, 9.2, 0);
-vex::vision::signature YELLOW_GOAL = vex::vision::signature (1, 1947, 2657, 2302, -3797, -3427, -3612, 3, 0);
+vex::vision::signature YELLOW_GOAL = vex::vision::signature (3, 2065, 2457, 2262, -3433, -3209, -3322, 6.8, 0);
 vex::vision::signature SIG_4 = vex::vision::signature (4, 0, 0, 0, 0, 0, 0, 2.5, 0);
 vex::vision::signature SIG_5 = vex::vision::signature (5, 0, 0, 0, 0, 0, 0, 2.5, 0);
 vex::vision::signature SIG_6 = vex::vision::signature (6, 0, 0, 0, 0, 0, 0, 2.5, 0);
 vex::vision::signature SIG_7 = vex::vision::signature (7, 0, 0, 0, 0, 0, 0, 2.5, 0);
-vex::vision vis = vex::vision (vex::PORT1, 36, RED_GOAL, BLUE_GOAL, YELLOW_GOAL, SIG_4, SIG_5, SIG_6, SIG_7);
+vex::vision vis = vex::vision (vex::PORT1, 27, RED_GOAL, BLUE_GOAL, YELLOW_GOAL, SIG_4, SIG_5, SIG_6, SIG_7);
 /*vex-vision-config:end*/
